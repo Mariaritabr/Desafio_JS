6e2d640a73c95a757492976ecee7b0424132baf7
Mariaritabr <mrbraz06@gmail.com>
2023-05-23 16:20:32 -0300

    first commit

---
+++ orange.js
@@ -0,0 +1,18 @@
+function adicioneseunome(){
+    let nome = "Maria rita";
+    console.log(nome)
+    document.getElementById('aparecer').innerHTML = nome
+}
+function adicioneidade(){
+    let idade = "16";
+    console.log(idade)
+    document.getElementById('aparecer').innerHTML = idade
+}
+    
+function adicioneemail(){
+    let email = "mariarita0@gmail.com"
+    console.log(email) 
+    document.getElementById('aparecer').innerHTML = email
+}
+
+
